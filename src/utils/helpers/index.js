0611f66efdca8165263f1c83e670bf2aab0c5456
EduardoTartas <eduardos.tartas@gmail.com>
2026-03-05 16:47:00 -0400

    feat(permission): função de verificação de permissão

--- src/utils/helpers/index.js
+++ src/utils/helpers/index.js
@@ -6,4 +6,5 @@ export { default as HttpStatusCodes } from './HttpStatusCodes.js';
 export { default as errorHandler } from './errorHandler.js';
 export { default as messages } from './messages.js';
 export { default as StatusService } from './StatusService.js';
+export { default as ensurePermission } from './PermissionHelper.js';
 export { default as asyncWrapper } from '../../middlewares/asyncWrapper.js';
