f6cc6a86ca4cd5006552459cca4ce855bbb06b26
Geovanna Rocha <rochageovanna3@gmail.com>
2026-04-24 21:01:58 -0400

    feat: implementa filtro por nome ignorando acentuação e filtro por ativo

--- src/repository/filters/AdicionalOpcaoFilterBuild.js
+++ src/repository/filters/AdicionalOpcaoFilterBuild.js
@@ -16,3 +16,33 @@ class AdicionalOpcaoFilterBuild {
         return this;
     }
 
+        comNome(nome) {
+        if (nome) {
+            const normalizeString = (str) => {
+                return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+            };
+            const normalizedNome = normalizeString(nome);
+            const escapeRegex = (string) => string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+            const pattern = escapeRegex(normalizedNome).replace(/a/g, '[aàáâãäå]')
+                                                        .replace(/e/g, '[eèéêë]')
+                                                        .replace(/i/g, '[iìíîï]')
+                                                        .replace(/o/g, '[oòóôõö]')
+                                                        .replace(/u/g, '[uùúûü]')
+                                                        .replace(/c/g, '[cç]')
+                                                        .replace(/n/g, '[nñ]');
+
+        this.filtros.nome = {
+                $regex: pattern,
+                $options: "i"
+            };
+        }
+        return this;
+    }
+    comAtivo(ativo = true) {
+            if (ativo !== undefined) {
+                const valor =
+                    ativo === true || ativo === "true" || ativo === 1 || ativo === "1";
+                this.filtros.ativo = valor;
+            }
+            return this;
+        }
